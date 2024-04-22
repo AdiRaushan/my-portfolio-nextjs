36e446687bc67a2cc7522c5fbe4ed4d9eb8d9df8
AdiRaushan <adiraushan2k@gmail.com>
2024-04-22 20:44:30 +0530

    icons

--- components/grid-items/socials.tsx
+++ components/grid-items/socials.tsx
@@ -1,14 +1,25 @@
 import { GridItemInterface } from "@/config/site-config";
+import Icon from "../icon";
 
 const SocialBox = ({ item }: { item: GridItemInterface }) => {
     return <div>
-        {/* header */}
-        <div>
+        <div className="flex items-center justify-between">
             {/* icon */}
-            Icons
+            <Icon type={item.icon ?? ""} color={item.color} />
+            {/* button */}
+            {item.layout === "2x2" && <div>Button</div>}
         </div>
-        {item.layout === "2x2" && <div>Button</div>}
+        <div className="mt-2">
+            {/* content */}
+            <div className="text-lg font-semibold">{item.title}</div>
+            <div className="text-sm text-neutral-500">{item.username}</div>
+            {item.description && <div className="text-neutral-500 text-sm mt-1 ">{item.description}</div>}
+            {/* button */}
+            {item.layout === "1x2" && <div>button</div>}
+        </div>
+
     </div>
+
 }
 
 export default SocialBox
