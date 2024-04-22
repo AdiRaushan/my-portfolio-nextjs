0cfb11da5c64e465eb94e0714b6cb19e8c4f6630
AdiRaushan <adiraushan2k@gmail.com>
2024-04-22 19:43:25 +0530

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,63 @@
+import GridItem from "@/components/grid-item";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { siteConfig } from "@/config/site-config";
+import { Mail, MapPin } from "lucide-react";
 import Image from "next/image";
+
+
+
+
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-1 h-full w-full gap-10 ">
+      <div className="flex-1 h-full max-w-md p-8">
+        {/* Left side */}
+        <div className="flex flex-col  h-full space-y-6 rounded-md">
+
+          <Image
+            alt="avatar"
+            src="/adi-pf.png"
+            width="100"
+            height="100"
+            priority
+            loading="eager"
+          />
+          <div className="mt-6">
+            {/* Tittle */}
+            <div className="text-md font-semibold text-blue-300 ">{siteConfig.title}</div>
+            {/* Fullname */}
+            <h1 className="text-2xl mt-2 font-bold">{siteConfig.creator}</h1>
+            {/* bio */}
+            <p className="text-xl font-light text-neutral-500">{siteConfig.bio}</p>
+          </div>
+          {/* buttons */}
+          <div className="flex items-center justify-between gap-6 ">
+            <a className="text-sm cursor-pointer w-full border flex items-center border-neutral-200 dark:border-neutral-800 py-2 px-3 rounded-md gap-1"> <MapPin size={16} />{siteConfig.location}</a>
+
+            <a className="text-sm cursor-pointer w-full border flex items-center border-neutral-100 dark:border-neutral-800 py-2 px-3 rounded-md gap-1 " href={`mailto:${siteConfig.email}`}> <Mail size={16} />Contact me</a>
+          </div>
+
+          {/* footer */}
+          <div className="flex items-center justify-between pt-6 text-xs border-t border-neutral-100  dark:border-neutral-800">
+            <div className="text-neutral-500">Buit By Adi Raushan | @2024 WebDev, All Right Reserve</div>
+            <ThemeToggle />
+          </div>
+
         </div>
       </div>
+      <div className="flex-1 h-full p-3">
+        {/* Right side */}
+        <div className="w-full h-full overflow-y-auto p-6 grid grid-cols-4 auto-rows-[76px] gap-10">
+          {siteConfig.item.map((item, index) => {
+            return (
+              <GridItem key={item.title + index} size={item.layout}><div>{item.title}</div></GridItem>
+            )
+          })}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
   );
---
+++ components/grid-item.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { cn } from '@/utils/cn';
+import { cva, type VariantProps } from "class-variance-authority";
+
+
+const variants = cva(
+    "shadow-grid rounded-3xl bg-white dark:bg-neutral-900 flex flex-col justify-center @container",
+    {
+        variants: {
+            size: {
+                "1x2": "md:col-span-1 col-span-2 row-span-2 md:p-8 p-4",
+                "2x1": "md:col-span-2 col-span-full row-span-1 py-4 md:px-8 px-4",
+                "2x2": "md:col-span-2 col-span-full row-span-2 md:p-8 p-4",
+                "2x4":
+                    "md:col-span-2 col-span-full row-span-4 relative overflow-hidden",
+            },
+        },
+        defaultVariants: {
+            size: "1x2",
+        },
+    }
+);
+
+
+const GridItem = ({ size, children }: {
+    size: string;
+    children: React.ReactNode;
+}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default GridItem
