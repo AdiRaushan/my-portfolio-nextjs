fee7fe2b2fbbdfebb7e0bc652899130ae8a68b51
AdiRaushan <adiraushan2k@gmail.com>
2024-04-25 23:47:54 +0530

    some changes

--- components/grid-items/project.tsx
+++ components/grid-items/project.tsx
@@ -9,11 +9,11 @@ const Project = ({ item }: { item: GridItemInterface }) => {
         <Link className='flex items-center gap-3' href={item.buttonLink ?? ""}>{/* icon  */}
             <Icon type={item.icon ?? ""} color={item.color} />
             {/* title */}
-            <div className='w-full text-lg font-semibold'>{item.title}</div>
+            <div className='w-full @md:text-lg text-basefont-semibold'>{item.title}</div>
             {/* Links */}
             <div className=' flex items-center gap-1'>
 
-                <span className='mt-[1px]'>Visit</span>
+                <span className='mt-[1px] @md:text-lg text-base'>Visit</span>
                 <Star size="13" fill='currentColor' />
             </div>
         </Link>
