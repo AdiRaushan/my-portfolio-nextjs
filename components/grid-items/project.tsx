14637ca88760713fd2975c4f3175989de49e33b4
AdiRaushan <adiraushan2k@gmail.com>
2024-04-26 00:30:46 +0530

    new update

--- components/grid-items/project.tsx
+++ components/grid-items/project.tsx
@@ -2,21 +2,22 @@ import { GridItemInterface } from "@/config/site-config";
 import Link from "next/link";
 import React from "react";
 import Icon from "../icon";
-import { Star } from "lucide-react";
+import { ExternalLink } from "lucide-react";
 
 const Project = ({ item }: { item: GridItemInterface }) => {
     return (
-        <Link className="flex items-center gap-3" href={item.buttonLink ?? ""}>
+        <Link className="flex items-start mb-10" href={item.buttonLink ?? ""}>
             {/* icon  */}
             <Icon type={item.icon ?? ""} color={item.color} />
             {/* title */}
-            <div className="w-full @md:text-lg text-basefont-semibold">
+            <div className="w-full @md:text-lg text-basefont-semibold mb-3          ">
                 {item.title}
             </div>
+            {item.description && <div className="text-neutral-500 text-sm mt-1 line-clamp-2 @lg:line-champ-none ">{item.description}</div>}
             {/* Links */}
-            <div className=" flex items-center gap-1">
-                <span className="mt-[1px] @md:text-lg text-base">Visit</span>
-                <Star size="13" fill="currentColor" />
+            <div className=" flex  gap-1">
+                <span className="mt-[15px] @md:text-lg text-base">Visit</span>
+                < ExternalLink size="15" className="mt-3" />
             </div>
         </Link>
     );
